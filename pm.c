fe1a5b2231187e8f95d1354406a19e96fbf87a2e
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-01-25 11:13:40 +0000

    Create pm.c

---
+++ pm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int n,i,a,count=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i,n;i++)
+{
+	a=n%i;
+}
+if(a==0)
+{
+	count++;
+}
+if((count==0)||(count==1))
+{
+	printf("%d is prime number",n);
+}
+else
+{
+printf("%d is not a prime number",n);
+}
+
+	return 0;
+}
