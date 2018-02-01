2e0267c6b4c426a2e35699b1e807fee52d24fc2d
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-01 14:38:28 +0530

    Rename word.c to letter.c

---
+++ letter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char s(200);
+	int count=0,i;
+scanf("%s",&s);
+	for(i=0;s(i)!='\0';i++)
+	{
+		if(s[i]==' ')
+		count++;
+	}
+	printf("%d",count+1);
+
+
+	return 0;
+}
