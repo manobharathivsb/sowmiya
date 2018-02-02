e62b73cc47e997f2f6e22ee06a6a3596aaf7beb7
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-02 14:32:47 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+	char a[6],b[6];
+	int i;
+	a[6]=madam;
+	strcpy(b,a);
+	strrev(a);
+	i==strcmp(a,b);
+	if(i==0)
+	{
+		printf("it is a palindrome");
+	}
+else
+{
+ printf("it is not a palindrome");
+}
+	return 0;
+}
