e0eb4c12cd1a133e7456e3cd105b740e5ac521de
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-04 20:15:53 +0530

    Create Team.c

---
+++ Team.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+Scanf("%d%d%d",&a,&b,&c);
+Printf("enter the number of students in the class:%d",a);
+Printf("enter the number of teams:%d",b);
+Printf("enter the number of students in each team:%d",c);
+{
+s=a-(b*c);
+Printf("left of students:%d",s);
+}
+return 0;
+}
