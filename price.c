7bee3a5567d54a89dfe6f8713305a3a664e19cab
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-04 16:55:41 +0530

    Create price.c

---
+++ price.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void) 
+{
+	int a,b,c,sub,total;
+	scanf("%d",&n)
+	scanf("%d%d%d",&a,&b,&c);
+	if(n==1)
+	{
+	printf("VSB stationary\n");
+	printf("first item price:%d\n",a);
+	printf("second item price:%d\n",b);
+	{
+		total=a+b;
+		printf("total price:%d\n",total);
+	}
+	printf("amount given by student customer:%d\n",c);
+	{
+		sub=c-total;
+	}
+	printf("amount returned by shopkeeper:%d\n",sub);
+	}
+	else
+	{
+		printf("student doesn't need receipt);
+	}
+	return 0;
+	}
+
+
