062406568f521f17f62afa280915f714e18607fe
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-04 11:49:46 +0530

    Create func.c

---
+++ func.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int sample();
+int main()
+{
+	printf("welcome to vsb\n");
+	sample();
+	printf("&&&&&&&&&&&&&&&&&&&&&\n");
+	printf("return to main function");
+	return 0;
+}
+sample()
+{
+	printf("EEE department\n");
+	printf("****************\n");
+}
+
+
