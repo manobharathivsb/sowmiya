d99d56fad321a0c473a03d525af2e08c1a16b4a9
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-09 14:51:30 +0530

    Create diff.c

---
+++ diff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+int min1,min2,hrs1,hrs2,sub,diff;
+scanf("%d\t%d",&hrs1,&min1);
+scanf("%d\t%d",&hrs2,&min2);
+{
+	diff=hrs1-hrs2;
+	sub=min1-min2;
+
+}
+	printf("%d\t%d",diff,sub);
+return 0;
+}
