6b9bbbd7b36e92fb4f2d80e81eaf5a0dc2a69df7
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-20 11:00:04 +0000

    Create p.c

---
+++ p.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("it is a power of 2");
+	}
+		else
+		{
+			printf("it is not a power  of 2");
+	}
+	return 0;
+}
