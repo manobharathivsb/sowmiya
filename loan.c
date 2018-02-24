24b3c215193eef18ae4a0aec1f858cd30498d9e9
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-24 12:20:30 +0530

    Create loan.c

---
+++ loan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) 
+{
+	int a,b,c,n,i,salary;
+	scanf("%d",&i);
+	scanf("%d\t%d",&a,&b);
+	scanf("%d",&salary);
+i==1;
+{
+	if(a>=5)
+	{
+	printf("employee must have completed atleast %d\n years in the organisation",a);
+	if(b==0)
+	printf("employee must not haven any other loan %d\n",b);
+	if(c==salary<3*5000)
+	printf("the loan requird should be less than 3 months salary %d\n",c);
+}
+else
+{
+	printf("the loan is rejected");
+}
+}
+return 0;
+}
